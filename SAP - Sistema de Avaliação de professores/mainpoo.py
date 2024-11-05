0b70ee1e8ccf3daa11c184946218079400f6c1c8
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 02:54:18 -0400

    Update mainpoo.py

--- SAP - Sistema de Avaliação de professores/mainpoo.py
+++ SAP - Sistema de Avaliação de professores/mainpoo.py
@@ -5,7 +5,11 @@ from ClasseProfessor import*
 from ClasseUsuario import*
 from dicionario import*
 from instancias import*
-mural = Mural()
+from ClasseMural import*
+from ClasseAvaliacao import*
+from ClasseChefia import*
+
+ mural = Mural()
 
 while True:
   print("-" * 12, "MENU DO SAP", "-" * 12)
@@ -13,7 +17,7 @@ while True:
   opcao = input(">")
 
   if opcao == "1":
-    print("\nCadastrar como:\n[1] - Aluno\n[2] - Professor")
+    print("\nCadastrar como:\n[1] - Aluno\n[2] - Professor\n[3] - Chefe Imediato")
     cadastrar_usuario = input("Coloque sua resposta:\n> ")
 
     if cadastrar_usuario == "1":
@@ -27,11 +31,18 @@ while True:
       criar_usuario_obj.cadastrar()
       dicionario_de_profs_cadastrados[criar_usuario_obj.get_login_usuario()] = f"{criar_usuario_obj.get_nome()}", criar_usuario_obj.get_idade(), f"{criar_usuario_obj.get_login_usuario()}", f"{criar_usuario_obj.get_login_senha()}", criar_usuario_obj.get_salario() , f"{criar_usuario_obj.get_disciplina_ministrada()}"
       print(f"Você foi cadastrado {criar_usuario_obj.get_nome()}")
+
+    elif cadastrar_usuario == "3":
+      criar_usuario_obj = ChefiaImediata(0,0,0,0,0,0)
+      criar_usuario_obj.cadastrar()
+      dicionario_de_chefia[criar_usuario_obj.get_login_usuario()] = f"{criar_usuario_obj.get_nome()}", criar_usuario_obj.get_idade(), f"{criar_usuario_obj.get_login_usuario()}", f"{criar_usuario_obj.get_login_senha()}", criar_usuario_obj.get_idChefia() , f"{criar_usuario_obj.get_email()}"
+      print(f"Você foi cadastrado {criar_usuario_obj.get_nome()}")
     else:
       print("Coloque uma resposta válida")
 
+
   elif opcao == "2":
-    print("\nRealizar login como:\n[1] - Aluno\n[2] - Professor")
+    print("\nRealizar login como:\n[1] - Aluno\n[2] - Professor\n[3] - Chefe Imediato")
     tipo_usuario = input("Coloque sua resposta:\n> ")
     if tipo_usuario == "1":
       login_usuario = input("Coloque seu usuário para login:")
@@ -42,18 +53,31 @@ while True:
       if opcao_escrever == "1":
         usuario = dicionario_de_usuario_alunos_cadastrados[login_usuario]
         Aluno_logado = Aluno (usuario[0],usuario[1],usuario[2],usuario[3],usuario[4],usuario[5])
-        conteudo = Aluno_logado.escreverAvaliacao()                                                            
-        avaliacao = Avaliacao(0, conteudo , conteudo, Aluno_logado, 0)                                           #Associação     
-        mural.adicionarAvaliacao(avaliacao)                                                                      #Agregação
-        mural.consultarAvaliacao()
-        nome_profs = [valor[0] for valor in dicionario_de_profs_cadastrados.values()]
-        print (nome_profs)
-        DadoProfessor = input("Por favor, coloque o nome do professor a quem deseja avaliar")  #OBS: INVERTER POSIÇÕES, PARA PERGUNTAR A QUE PROF DESEJA ENVIAR PRIMEIRO E DEPOIS AVALIAR
-        if DadoProfessor not in nome_profs:
+        usuario_profs = [professor for professor in dicionario_de_profs_cadastrados.keys()]
+        print (usuario_profs)
+        DadoProfessor = input("Por favor, coloque o usuário do professor a quem deseja avaliar")  #OBS: INVERTER POSIÇÕES, PARA PERGUNTAR A QUE PROF DESEJA ENVIAR PRIMEIRO E DEPOIS AVALIAR
+        Professor1 = dicionario_de_profs_cadastrados[DadoProfessor]
+        ProfessorCitado = Professor(Professor1[0],Professor1[1],Professor1[2],Professor1[3],Professor1[4],Professor1[5])
+        if DadoProfessor not in usuario_profs:
           print ("Coloque uma resposta válida")
           continue
         else:
-          avaliacao = Avaliacao(0, conteudo , conteudo, Aluno_logado, DadoProfessor)
+          usuario = dicionario_de_usuario_alunos_cadastrados[login_usuario]
+          Aluno_logado = Aluno (usuario[0],usuario[1],usuario[2],usuario[3],usuario[4],usuario[5])
+          usuario_profs = [professor for professor in dicionario_de_profs_cadastrados.keys()]
+          print (usuario_profs)
+          DadoProfessor = input("Por favor, coloque o usuário do professor a quem deseja avaliar")  #OBS: INVERTER POSIÇÕES, PARA PERGUNTAR A QUE PROF DESEJA ENVIAR PRIMEIRO E DEPOIS AVALIAR
+          Professor1 = dicionario_de_profs_cadastrados[DadoProfessor]
+          ProfessorCitado = Professor(Professor1[0],Professor1[1],Professor1[2],Professor1[3],Professor1[4],Professor1[5])
+          if DadoProfessor not in usuario_profs:
+            print ("Coloque uma resposta válida")
+            continue
+          else:
+            conteudo = Aluno_logado.escreverAvaliacao()                                                            
+            avaliacao = Avaliacao(0, conteudo , conteudo, Aluno_logado, ProfessorCitado)                                         #Associação     
+            mural.adicionarAvaliacao(avaliacao)                                                                      #Agregação
+            mural.consultarAvaliacao()
+
           print("Sua avaliação está completa")
 
     elif tipo_usuario == "2":
@@ -61,10 +85,14 @@ while True:
       login_senha = input("Coloque sua senha para login:")
       logar(login_usuario, login_senha, dicionario_de_profs_cadastrados)
 
+    elif tipo_usuario == "3":
+      login_usuario = input("Coloque seu usuário para login:")
+      login_senha = input("Coloque sua senha para login:")
+      logar(login_usuario, login_senha, dicionario_de_chefia)
+      
   elif opcao == "3":
     print("Bem, Adeus")
     break
-
 
 
 
