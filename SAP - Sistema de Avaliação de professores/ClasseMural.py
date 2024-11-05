3c4972325b0205b5612e27d5695497a3c923b937
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 16:04:44 -0400

    Update ClasseMural.py

--- SAP - Sistema de Avaliação de professores/ClasseMural.py
+++ SAP - Sistema de Avaliação de professores/ClasseMural.py
@@ -1,13 +1,15 @@
 class Mural:
-    def __init__(self, idMural, quantidadeAvaliacao, descricao, avaliacoesAprovados):
-        self.idMural = idMural
-        self.quantidadeAvaliacao = quantidadeAvaliacao
-        self.descricao = descricao
-        self.avaliacoesAprovados = avaliacoesAprovados
+    def __init__(self):
+        self.idMural = None
+        self.quantidadeAvaliacao = None
+        self.descricao = None
+        self.avaliacoesAprovados = []
+
 
 #metodos
     def adicionarAvaliacao(self, Avaliacao):
         self.avaliacoesAprovados.append(Avaliacao)
+
 
     def removerAvaliacao(self, idAvaliacao):
         pass
@@ -16,6 +18,6 @@ class Mural:
         for avaliacao in self.avaliacoesAprovados:
             print (avaliacao.get_conteudoAvaliacao(),avaliacao.get_remetenteAvaliacao(), avaliacao.get_destinatarioAvaliacao(), avaliacao.get_respostaAvaliacao())
         print("*"*40)
-
     def consultarDescricao(self):
         pass
+
