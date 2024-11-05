aad06c26f03747f6a84ef6a7e49780e6823330c2
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 16:04:07 -0400

    Update ClasseChefia.py

--- SAP - Sistema de Avaliação de professores/ClasseChefia.py
+++ SAP - Sistema de Avaliação de professores/ClasseChefia.py
@@ -1,30 +1,40 @@
-from ClasseAluno import*
-from ClasseProfessor import*
 from ClasseUsuario import*
-from dicionario import*
-from instancias import*
-from ClasseAvaliacao import *
+from random import *
 
 class ChefiaImediata(UsuarioIfro):
     def __init__(self, nome, idade, login_usuario,login_senha, idChefia, email):
-        super().__init__(self, nome, idade, login_senha, login_usuario)
+        super().__init__(nome, idade, login_usuario,login_senha)
         self.idChefia = idChefia
         self.email = email
-        self.Avaliacao = Avaliacao
+        self.Avaliacao = None
+
+
     def get_idChefia(self):
         return self.idChefia
-    
+   
     def get_email(self):
         return self.email
-    
+   
     def set_idChefia(self, novoID):
         self.idChefia = novoID
+
 
     def set_email(self, novoEmail):
         self.email = novoEmail
 
+
     def aprovarAvaliacao(self, Avaliacao):
         pass
 
+
     def reprovarAvaliacao(self):
         pass
+
+
+    def cadastrar(self):
+     self.nome = input("Digite seu nome: ")
+     self.idade = int(input("Digite sua idade:"))
+     self._login_usuario = input("Cadastre um usuário: ")
+     self._login_senha = input("Cadastre uma senha: ")
+     self.idChefia = print(randint(100, 300))
+     self.disciplina_ministrada = input("Digite seu email: ")
