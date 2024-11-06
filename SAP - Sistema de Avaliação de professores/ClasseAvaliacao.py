00b8abcb86166432757de784974ff8cc3bad78ce
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-06 01:03:32 -0400

    Update ClasseAvaliacao.py

--- SAP - Sistema de Avaliação de professores/ClasseAvaliacao.py
+++ SAP - Sistema de Avaliação de professores/ClasseAvaliacao.py
@@ -1,57 +1,49 @@
 class Avaliacao:
-    def __init__(self, idAvaliacao, conteudoAvaliacao, dataehoraAvaliacao, remetenteAvaliacao, destinatarioAvaliacao):
-        self.idAvaliacao = idAvaliacao
-        self.conteudoAvaliacao = conteudoAvaliacao
-        self.dataehoraAvaliacao = dataehoraAvaliacao
-        self.remetenteAvaliacao = remetenteAvaliacao
-        self.destinatarioAvaliacao = destinatarioAvaliacao
-        self.status = None
-        self.respostaAvaliacao = None
-        
+    def __init__(self, conteudoAvaliacao, dataehoraAvaliacao, remetenteAvaliacao, destinatarioAvaliacao):
+        self.__idAvaliacao = None
+        self.__conteudoAvaliacao = conteudoAvaliacao
+        self.__dataehoraAvaliacao = dataehoraAvaliacao                                                      
+        self.__remetenteAvaliacao = remetenteAvaliacao                                                  #Associação
+        self.__destinatarioAvaliacao = destinatarioAvaliacao                                            #Associação
+        self.__status = None
+
     def get_idAvaliacao(self):  
-        return f"\n\n\ID da avaliação: {self.idAvaliacao}\n\n"
+        return f"\n\nID da avaliação: {self.__idAvaliacao}\n\n"
         
     def get_conteudoAvaliacao(self):    
-        return self.conteudoAvaliacao
+        return self.__conteudoAvaliacao
         
     def get_dataehoraAvaliacao(self):
-        return self.dataehoraAvaliacao
+        return self.__dataehoraAvaliacao
 
     def get_remetenteAvaliacao(self):
-        return f"\n\nRemetente da avaliação: {self.remetenteAvaliacao.get_nome()}\nAno escolar do Remetente: {self.remetenteAvaliacao.get_ano_escolar()}\nCurso do Remetente: {self.remetenteAvaliacao.get_curso()}"
+        return f"\n\nRemetente da avaliação: {self.__remetenteAvaliacao.get_nome()}\nAno escolar do Remetente: {self.__remetenteAvaliacao.get_ano_escolar()}\nCurso do Remetente: {self.__remetenteAvaliacao.get_curso()}"
 
     def get_destinatarioAvaliacao(self):
-        return f"\n\nDestinatário da avaliação: {self.destinatarioAvaliacao.get_nome()}\nDisciplina ministrada pelo Destinatário: {self.destinatarioAvaliacao.get_disciplina_ministrada()}"
-    
-    def get_respostaAvaliacao(self):
-        if self.respostaAvaliacao == None:
-            return "\n\nResposta do Destinatário:\nA avaliação ainda não foi respondida pelo Destinatário\n\n"
-        else:    
-            return f"\n\nResposta do Destinatário:\n{self.respostaAvaliacao}\n\n"
+        return f"\n\nDestinatário da avaliação: {self.__destinatarioAvaliacao.get_nome()}\nDisciplina ministrada pelo Destinatário: {self.__destinatarioAvaliacao.get_disciplina_ministrada()}"
     
     def set_idAvaliacao(self, idAvaliacao):
-        self.idAvaliacao = idAvaliacao
+        self.__idAvaliacao = idAvaliacao
 
     def set_conteudoAvaliacao(self, conteudoAvaliacao):
-        self.conteudoAvaliacao = conteudoAvaliacao
+        self.__conteudoAvaliacao = conteudoAvaliacao
 
-    def set_horaAvaliacao(self, horaAvaliacao):
-        self.horaAvaliacao = horaAvaliacao
+    def set_dataehoraAvaliacao(self, dataehoraAvaliacao):
+        self.__dataehoraAvaliacao = dataehoraAvaliacao
 
     def set_remetenteAvaliacao(self, remetenteAvaliacao):
-        self.remetenteAvaliacao = remetenteAvaliacao
+        self.__remetenteAvaliacao = remetenteAvaliacao
 
     def set_destinatarioAvaliacao(self, destinatarioAvaliacao):
-        self.destinatarioAvaliacao = destinatarioAvaliacao
-
-    def set_respostaAvaliacao(self, respostaAvaliacao):
-        self.respostaAvaliacao = respostaAvaliacao
+        self.__destinatarioAvaliacao = destinatarioAvaliacao
 
     def gerarAvaliacao(self):
-        return (f"{self.get_idAvaliacao()}{self.get_remetenteAvaliacao()}{self.get_destinatarioAvaliacao()}{self.get_respostaAvaliacao()}")
+        return (f"{self.__get_idAvaliacao()}{self.__get_remetenteAvaliacao()}{self.__get_destinatarioAvaliacao()}{self.__get_respostaAvaliacao()}")
 
     def consultarStatus(self):
-        if self.status == None:
+        if self.__status == None:
             print ("A avaliação ainda não foi revisado.\nStatus: Em Andamento")
         else:
-            print (f"Status da avaliação: {self.status}")
+            print (f"Status da avaliação: {self.__status}")
+
+
