be28b7382a2b5eb22c8b398bba2310d22c19ac8b
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 21:24:28 -0400

    Update ClasseMural.py

--- SAP - Sistema de Avaliação de professores/ClasseMural.py
+++ SAP - Sistema de Avaliação de professores/ClasseMural.py
@@ -4,20 +4,19 @@ class Mural:
         self.quantidadeAvaliacao = None
         self.descricao = None
         self.avaliacoesAprovados = []
-
-
-#metodos
+    
     def adicionarAvaliacao(self, Avaliacao):
         self.avaliacoesAprovados.append(Avaliacao)
-
-
+        
     def removerAvaliacao(self, idAvaliacao):
         pass
+        
     def consultarAvaliacao(self):
-        print("*"*40)
+        print("-"*100)
         for avaliacao in self.avaliacoesAprovados:
             print (avaliacao.get_conteudoAvaliacao(),avaliacao.get_remetenteAvaliacao(), avaliacao.get_destinatarioAvaliacao(), avaliacao.get_respostaAvaliacao())
-        print("*"*40)
+        print("-"*100)
+        
     def consultarDescricao(self):
         print("O mural é o local onde o usuário pode inserir suas avaliações feitas para um docente de sua instituição e ver outras avaliações realizadas por outros discentes.")
 
