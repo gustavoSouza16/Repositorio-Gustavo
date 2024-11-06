94aac7b989187bac4273cc36af8332d6be7cb7b5
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 21:30:51 -0400

    Update ClasseProfessor.py

--- SAP - Sistema de Avaliação de professores/ClasseProfessor.py
+++ SAP - Sistema de Avaliação de professores/ClasseProfessor.py
@@ -1,5 +1,4 @@
-from ClassesUsuraio import *
-
+from classes import *
 
 class Professor(UsuarioIfro):
     def __init__(self, nome, idade, login_usuario,login_senha, salario, disciplina_ministrada):
@@ -7,22 +6,17 @@ class Professor(UsuarioIfro):
         self._salario = salario
         self.disciplina_ministrada = disciplina_ministrada
 
-
     def get_salario(self):
         return(self._salario)
-
 
     def set_salario(self,novo_salario):
         self._salario = novo_salario
 
-
     def get_disciplina_ministrada(self):
         return(self.disciplina_ministrada)
 
-
     def set_disciplina_ministrada(self,nova_disciplina,):
         self.disciplina_ministrada = nova_disciplina
-
 
     def cadastrar(self):
         self.nome = input("Digite seu nome: ")
@@ -32,8 +26,5 @@ class Professor(UsuarioIfro):
         self._salario = int(input("Digite seu salário: "))
         self.disciplina_ministrada = input("Digite a disciplina que você ministra: ")
 
-
     def responderAvaliacao(self, avaliacao):
-
-
         pass
