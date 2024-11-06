5bfffd6eabab2a54eaea383c39ab75bf5c9fad39
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 21:32:18 -0400

    Update ClasseUsuario.py

--- SAP - Sistema de Avaliação de professores/ClasseUsuario.py
+++ SAP - Sistema de Avaliação de professores/ClasseUsuario.py
@@ -1,74 +1,37 @@
 from abc import ABC, abstractmethod
 
-
-
-
 class UsuarioIfro(ABC):
-
-
-
-
     def __init__(self, nome, idade,login_usuario,login_senha):
             self.nome = nome
             self.idade = idade
             self._login_usuario = login_usuario
             self._login_senha = login_senha
 
-
-
-
     def get_nome (self):
         return self.nome
-
-
-
-
+        
     def set_nome(self,nome):
         self.nome = nome
-
-
-
-
+        
     def get_idade(self):
         return self.idade
-
-
-
-
+        
     def set_idade (self, idade):
         self.idade = idade
-
-
-
-
+        
     def get_login_usuario(self):
         return self._login_usuario
-
-
-
-
+        
     def set_login_usuario(self, novo_login):
         self._login_usuario = novo_login
-
-
-
-
+        
     def get_login_senha(self):
         return self._login_senha
-
-
-
-
+        
     def set_login_senha(self,nova_senha):
         self._login_senha = nova_senha
 
-
-
-
     @abstractmethod
-
-
-
-
+    
     def cadastrar ():
         pass
