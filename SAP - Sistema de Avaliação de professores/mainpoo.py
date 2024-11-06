d39a767f6bf5934777405bd7863a71b3bd96c102
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 21:36:15 -0400

    Update mainpoo.py

--- SAP - Sistema de Avaliação de professores/mainpoo.py
+++ SAP - Sistema de Avaliação de professores/mainpoo.py
@@ -1,14 +1,14 @@
 #2°Ano Vespertino - Informática, Programação Orientada a Objetos
 #Integrantes: Gabriel Lima Rosa, Gustavo Neves, Luiz Guilherme, Wellington Pereira
-from classes import*
-from database import*
-from objetosPoo import*
-from classealunoo import Aluno
-from classeavaliacaoo import Avaliacao
+from ClasseUsuario import*
+from dicionario import*
+from instancias import*
+from ClasseAluno import Aluno
+from ClasseAvaliacao import Avaliacao
 from datetime import datetime
-from classemural import*
-from classechefia import *
-from classeprofessor import*
+from ClasseMural import*
+from ClasseChefia import *
+from ClasseProfessor import*
 
 
 def VasculharNomeProfessor():
