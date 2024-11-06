190466dc46008b7e3d3db119cc2fb78a68606f9e
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-05 21:28:56 -0400

    Update ClasseAvaliacao.py

--- SAP - Sistema de Avaliação de professores/ClasseAvaliacao.py
+++ SAP - Sistema de Avaliação de professores/ClasseAvaliacao.py
@@ -5,29 +5,30 @@ class Avaliacao:
         self.dataehoraAvaliacao = dataehoraAvaliacao
         self.remetenteAvaliacao = remetenteAvaliacao
         self.destinatarioAvaliacao = destinatarioAvaliacao
+        self.status = None
         self.respostaAvaliacao = None
-
+        
     def get_idAvaliacao(self):  
-        return self.idAvaliacao
-
+        return f"\n\n\ID da avaliação: {self.idAvaliacao}\n\n"
+        
     def get_conteudoAvaliacao(self):    
         return self.conteudoAvaliacao
-
-    def get_horaAvaliacao(self):
-        return self.horaAvaliacao
+        
+    def get_dataehoraAvaliacao(self):
+        return self.dataehoraAvaliacao
 
     def get_remetenteAvaliacao(self):
         return f"\n\nRemetente da avaliação: {self.remetenteAvaliacao.get_nome()}\nAno escolar do Remetente: {self.remetenteAvaliacao.get_ano_escolar()}\nCurso do Remetente: {self.remetenteAvaliacao.get_curso()}"
 
     def get_destinatarioAvaliacao(self):
         return f"\n\nDestinatário da avaliação: {self.destinatarioAvaliacao.get_nome()}\nDisciplina ministrada pelo Destinatário: {self.destinatarioAvaliacao.get_disciplina_ministrada()}"
-
+    
     def get_respostaAvaliacao(self):
         if self.respostaAvaliacao == None:
-            return "\n\nResposta do Destinatário:\nA avaliação ainda não foi respondida pelo Destinatário"
+            return "\n\nResposta do Destinatário:\nA avaliação ainda não foi respondida pelo Destinatário\n\n"
         else:    
-            return f"\n\nResposta do Destinatário:\n{self.respostaAvaliacao}"
-
+            return f"\n\nResposta do Destinatário:\n{self.respostaAvaliacao}\n\n"
+    
     def set_idAvaliacao(self, idAvaliacao):
         self.idAvaliacao = idAvaliacao
 
@@ -46,11 +47,11 @@ class Avaliacao:
     def set_respostaAvaliacao(self, respostaAvaliacao):
         self.respostaAvaliacao = respostaAvaliacao
 
-
-    def gerarAvaliacao(self,dAvaliacao, conteudoAvaliacao, dataAvaliacao, horaAvaliacao, remetenteAvaliacao, destinatarioAvaliacao, respostaAvaliacao):
-        Avaliacao(dAvaliacao, conteudoAvaliacao, dataAvaliacao, horaAvaliacao, remetenteAvaliacao, destinatarioAvaliacao, respostaAvaliacao)
+    def gerarAvaliacao(self):
+        return (f"{self.get_idAvaliacao()}{self.get_remetenteAvaliacao()}{self.get_destinatarioAvaliacao()}{self.get_respostaAvaliacao()}")
 
     def consultarStatus(self):
         if self.status == None:
             print ("A avaliação ainda não foi revisado.\nStatus: Em Andamento")
-        print (f"Status da avaliação: {self.status}")
+        else:
+            print (f"Status da avaliação: {self.status}")
