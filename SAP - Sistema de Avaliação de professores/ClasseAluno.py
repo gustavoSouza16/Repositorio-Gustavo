58c32ae2da188245644c0ca2f71fe8057c41d8ff
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-06 01:03:14 -0400

    Update ClasseAluno.py

--- SAP - Sistema de Avaliação de professores/ClasseAluno.py
+++ SAP - Sistema de Avaliação de professores/ClasseAluno.py
@@ -5,34 +5,38 @@ class Aluno(UsuarioIfro):
 
     def __init__(self, nome, idade, login_usuario,login_senha,ano_escolar, curso):
         super().__init__(nome, idade, login_usuario,login_senha)
-        self._ano_escolar = ano_escolar
-        self._curso = curso
+        self.__ano_escolar = ano_escolar
+        self.__curso = curso
 
     def get_ano_escolar(self):
-        return self._ano_escolar
+        return self.__ano_escolar
 
     def set_ano_escolar(self, novo_ano_escolar):
-        self._ano_escolar = novo_ano_escolar        
+        self.__ano_escolar = novo_ano_escolar        
         
     def get_curso(self):
-        return self._curso
+        return self.__curso
         
     def set_curso(self, novo_curso):
-        self._curso = novo_curso
+        self.__curso = novo_curso
         
     def cadastrar(self):
         while True:
-            self.nome = input("Digite seu nome: ")
-            self.idade = int(input("Digite sua idade:"))
-            self._login_usuario = input("Cadastre um usuário: ")
-            self._login_senha = input("Cadastre uma senha: ")
-            self._ano_escolar = input("Qual sua série?\n[1] - Primeiro ano\n[2] - Segundo ano\n[3] - Terceiro ano\n> ")
-            if self._ano_escolar != "1" and self._ano_escolar != "2" and self._ano_escolar != "3":
+            nome = input("Digite seu nome: ")
+            self.set_nome(nome)
+            idade = int(input("Digite sua idade:"))
+            self.set_idade(idade)
+            login_usuario = input("Cadastre um usuário: ")
+            self.set_login_usuario(login_usuario)
+            login_senha = input("Cadastre uma senha: ")
+            self.set_login_senha(login_senha)
+            self.__ano_escolar = input("Qual sua série?\n[1] - Primeiro ano\n[2] - Segundo ano\n[3] - Terceiro ano\n> ")
+            if self.__ano_escolar != "1" and self.__ano_escolar != "2" and self.__ano_escolar != "3":
                 print ("Coloque uma resposta válida.")
                 continue
             print ("Digite seu curso o nome do seu curso:\nInformática\nQuímica\nEdificações\nEletrotécnica")
-            self._curso = input("Coloque sua resposta: ").upper()
-            if self._curso != 'INFORMÁTICA' and self._curso != 'QUÍMICA' and self._curso != 'EDIFICAÇÕES' and  self._curso != 'ELETROTÉCNICA':
+            self.__curso = input("Coloque sua resposta: ").upper()
+            if self.__curso != 'INFORMÁTICA' and self.__curso != 'QUÍMICA' and self.__curso != 'EDIFICAÇÕES' and  self.__curso != 'ELETROTÉCNICA':
                 print ("Coloque uma opção válida")
                 continue
             else:
