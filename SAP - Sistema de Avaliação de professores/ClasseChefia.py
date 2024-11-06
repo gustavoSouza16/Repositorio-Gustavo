e730e4f94089a4fe19ddb9bcf7347207c212f8e8
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-06 01:03:49 -0400

    Update ClasseChefia.py

--- SAP - Sistema de Avaliação de professores/ClasseChefia.py
+++ SAP - Sistema de Avaliação de professores/ClasseChefia.py
@@ -2,37 +2,42 @@ from ClasseUsuario import*
 from random import *
 
 class ChefiaImediata(UsuarioIfro):
-    def __init__(self, nome, idade, login_usuario,login_senha, idChefia, email):
+    def __init__(self, nome, idade, login_usuario,login_senha, email):
         super().__init__(nome, idade, login_usuario,login_senha)
-        self.idChefia = idChefia
-        self.email = email
+        self.__email = email
 
     def get_idChefia(self):
-        return self.idChefia
+        return self.__idChefia
 
     def get_email(self):
-        return self.email
+        return self.__email
 
     def set_idChefia(self, novoID):
-        self.idChefia = novoID
+        self.__idChefia = novoID
 
     def set_email(self, novoEmail):
-        self.email = novoEmail
+        self.__email = novoEmail
 
     def visualizarAvaliacao(self,avaliacao):
-        return (f"{avaliacao.get_idAvaliacao()}\n{avaliacao.remetenteAvaliacao()}{avaliacao.get_destinatarioAvaliacao()}{avaliacao.get_respostaAvaliacao()}")
+        return avaliacao.gerarAvaliacao()                                                               #Associação
 
-    def aprovarAvaliacao(self, Avaliacao):
-        pass
+    def aprovarAvaliacao(self, mural,avaliacao):
+        print("Avaliação aprovada e sendo inserida no Mural")
+        mural.adicionarAvaliacao(avaliacao)                                                             #Agregação
+        print ("Avaliação inserida e disponível para os professores destinatários vizualizarem")
         
     def reprovarAvaliacao(self):
-        pass
+        print ("A Avaliação foi reprovada.")
 
     def cadastrar(self):
-        self.nome = input("Digite seu nome: ")
-        self.idade = int(input("Digite sua idade:"))
-        self._login_usuario = input("Cadastre um usuário: ")
-        self._login_senha = input("Cadastre uma senha: ")
-        self.idChefia = print(randint(100, 300))
-        self.disciplina_ministrada = input("Digite seu email: ")
+        nome = input("Digite seu nome: ")
+        self.set_nome(nome)
+        idade = int(input("Digite sua idade:"))
+        self.set_idade(idade)
+        login_usuario = input("Cadastre um usuário: ")
+        self.set_login_usuario(login_usuario)
+        login_senha = input("Cadastre uma senha: ")
+        self.set_login_senha(login_senha)
+        email = input("Digite seu email: ")
+        self.set_email(email)
        
