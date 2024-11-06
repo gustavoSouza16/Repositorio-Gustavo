2dfa8522967d691c520eb2c22e9de3561c919bad
Luiz Guilherme <luiz.g.barbosa076@gmail.com>
2024-11-06 01:07:04 -0400

    Update mainpoo.py

--- SAP - Sistema de Avaliação de professores/mainpoo.py
+++ SAP - Sistema de Avaliação de professores/mainpoo.py
@@ -10,7 +10,6 @@ from ClasseMural import*
 from ClasseChefia import *
 from ClasseProfessor import*
 
-
 def VasculharNomeProfessor():
     for professores in dicionario_de_profs_cadastrados:
         professor = dicionario_de_profs_cadastrados[professores]
@@ -21,41 +20,35 @@ while True:
     print("-" * 12, "MENU DO SAP", "-" * 12)
     print("\n1 - Cadastrar Usuário\n2 - Logar com uma conta\n3 - Sair")
     opcao = input(">")
-
+    
     if opcao == "1":
         print("\nCadastrar como:\n[1] - Aluno\n[2] - Professor\n[3] - Chefia Imediata")
         cadastrar_usuario = input("Coloque sua resposta:\n> ")
-
         if cadastrar_usuario == "1":
             criar_usuario_obj = Aluno(0,0,0,0,0,0)
             criar_usuario_obj.cadastrar()
             dicionario_de_usuario_alunos_cadastrados[criar_usuario_obj.get_login_usuario()] = f"{criar_usuario_obj.get_nome()}", (criar_usuario_obj.get_idade()), f"{criar_usuario_obj.get_login_usuario()}", f"{criar_usuario_obj.get_login_senha()}", f"{criar_usuario_obj.get_ano_escolar()}", f"{criar_usuario_obj.get_curso()}"
             print(f"Você foi cadastrado {criar_usuario_obj.get_nome()}")
-
         elif cadastrar_usuario == "2":
             criar_usuario_obj = Professor(0,0,0,0,0,0)
             criar_usuario_obj.cadastrar()
             dicionario_de_profs_cadastrados[criar_usuario_obj.get_login_usuario()] = f"{criar_usuario_obj.get_nome()}", criar_usuario_obj.get_idade(), f"{criar_usuario_obj.get_login_usuario()}", f"{criar_usuario_obj.get_login_senha()}", criar_usuario_obj.get_salario() , f"{criar_usuario_obj.get_disciplina_ministrada()}"
             print(f"Você foi cadastrado {criar_usuario_obj.get_nome()}")
-
         elif cadastrar_usuario == "3":
             criar_usuario_obj = ChefiaImediata(0,0,0,0,0,0)
             criar_usuario_obj.cadastrar()
             dicionario_de_chefia[criar_usuario_obj.get_login_usuario()] = f"{criar_usuario_obj.get_nome()}", criar_usuario_obj.get_idade(), f"{criar_usuario_obj.get_login_usuario()}", f"{criar_usuario_obj.get_login_senha()}", criar_usuario_obj.get_idChefia() , f"{criar_usuario_obj.get_email()}"
             print(f"Você foi cadastrado {criar_usuario_obj.get_nome()}")
-       
         else:
             print("Coloque uma resposta válida")
        
     elif opcao == "2":
         print("\nRealizar login como:\n[1] - Aluno\n[2] - Professor\n[3] - Chefia Imediata")
         tipo_usuario = input("Coloque sua resposta:\n> ")
-        
         if tipo_usuario == "1":
             login_usuario = input("Coloque seu usuário para login:")
             login_senha = input("Coloque sua senha para login:")
             op = logar(login_usuario, login_senha,dicionario_de_usuario_alunos_cadastrados)
-            
             if op == True:
                 opcao_escrever = input("[1] - Escrever Avaliação de professor\n[2] - Retornar ao menu")
                 if opcao_escrever == "1":
@@ -70,18 +63,15 @@ while True:
                     continue
                 else:
                     continue
-                
                 if DadoProfessor not in usuario_profs:
                     print ("Coloque uma resposta válida")
                     continue
-                 
                 else:
                     conteudo = Aluno_logado.escreverAvaliacao()                                                                                              
                     avaliacao = Avaliacao(conteudo , conteudo, Aluno_logado, ProfessorCitado)                                         #Associação    
                     AvaliacoesAndamento.append(avaliacao)
                     mural.adicionarAvaliacao(avaliacao)                                                                               #Agregação
                     print("Sua avaliação está completa")
-
             if op == False:
                 continue
 
@@ -90,7 +80,6 @@ while True:
             login_senha = input("Coloque sua senha para login:")
             op = logar(login_usuario, login_senha, dicionario_de_profs_cadastrados)
             if op == True:
-
                 visu = input("[1] - Visualizar o Mural de Avaliações\n[2] - Retornar ao menu")
                 if visu == "1":
                     prof = dicionario_de_profs_cadastrados[login_usuario]
@@ -100,7 +89,6 @@ while True:
                     continue
                 else:
                     continue
-            
             if op == False:
                 continue
 
@@ -108,10 +96,8 @@ while True:
             login_usuario = input("Coloque seu usuário para login:")
             login_senha = input("Coloque sua senha para login:")
             op = logar(login_usuario, login_senha, dicionario_de_chefia)
-
             if op == True:
-                viwer = input("[1] - Revisar avaliações\n[2] - Retornar ao menu")
-                           
+                viwer = input("[1] - Revisar avaliações\n[2] - Retornar ao menu")   
                 if viwer == "1":
                     chefia = dicionario_de_chefia[login_usuario]
                     chefiaIm = ChefiaImediata(chefia[0],chefia[1],chefia[2],chefia[3],chefia[4],chefia[5])
