eb8a16f9ac304037e7bb605d8ea90ea2b27d7735
Gabriel Rosa <167138044+G4BR13l-R0S4@users.noreply.github.com>
2024-12-15 01:23:02 -0400

    Atualização na ClasseAluno.py
    Adicionei exceções do Tipo ValueError, IdentationError e Qualquer tipo de erro no final.
    ValueERROR: Para inserir sua idade como número inteir ao inves de colocar uma letra ou um número decimal
    IdentationERROR: avisar o usuário que houve um erro de indentação no código, assim pedidndo para corrigir.
    Apenas EXcept: Mostra uma mensagem onde diz que houuve um problema no código.

--- SAP - Sistema de Avaliação de professores/ClasseAluno.py
+++ SAP - Sistema de Avaliação de professores/ClasseAluno.py
@@ -1,49 +1,64 @@
-from ClasseUsuario import *
-from datetime import datetime
+#Colinha sobre Tipos de ERROS by Alura rsrsrs
+#NameError geralmente acontece quando tentamos usar uma variável que ainda não foi definida.
+#KeyError acontece ao buscarmos por uma chave inexistente dentro de uma estrutura como um dicionário. !!!!!!!!!!!!!!!!!!!!!!!!
 
-class Aluno(UsuarioIfro):
+try:
+    from ClasseUsuario import *
+    from datetime import datetime
 
-    def __init__(self, nome, idade, login_usuario,login_senha,ano_escolar, curso):
-        super().__init__(nome, idade, login_usuario,login_senha)
-        self.__ano_escolar = ano_escolar
-        self.__curso = curso
+    class Aluno(UsuarioIfro):
 
-    def get_ano_escolar(self):
-        return self.__ano_escolar
+        def __init__(self, nome, idade, login_usuario,login_senha,ano_escolar, curso):
+            super().__init__(nome, idade, login_usuario,login_senha)
+            self.__ano_escolar = ano_escolar
+            self.__curso = curso
 
-    def set_ano_escolar(self, novo_ano_escolar):
-        self.__ano_escolar = novo_ano_escolar        
-        
-    def get_curso(self):
-        return self.__curso
-        
-    def set_curso(self, novo_curso):
-        self.__curso = novo_curso
-        
-    def cadastrar(self):
-        while True:
-            nome = input("Digite seu nome: ")
-            self.set_nome(nome)
-            idade = int(input("Digite sua idade:"))
-            self.set_idade(idade)
-            login_usuario = input("Cadastre um usuário: ")
-            self.set_login_usuario(login_usuario)
-            login_senha = input("Cadastre uma senha: ")
-            self.set_login_senha(login_senha)
-            self.__ano_escolar = input("Qual sua série?\n[1] - Primeiro ano\n[2] - Segundo ano\n[3] - Terceiro ano\n> ")
-            if self.__ano_escolar != "1" and self.__ano_escolar != "2" and self.__ano_escolar != "3":
-                print ("Coloque uma resposta válida.")
-                continue
-            print ("Digite seu curso o nome do seu curso:\nInformática\nQuímica\nEdificações\nEletrotécnica")
-            self.__curso = input("Coloque sua resposta: ").upper()
-            if self.__curso != 'INFORMÁTICA' and self.__curso != 'QUÍMICA' and self.__curso != 'EDIFICAÇÕES' and  self.__curso != 'ELETROTÉCNICA':
-                print ("Coloque uma opção válida")
-                continue
-            else:
-                print ("Cadastro Finalizado")
-                break
-                
-    def escreverAvaliacao(self):
-        avalicacao_escrita = input("Escreva sua avaliação\n> ")
-        dataehora = datetime.now()
-        return avalicacao_escrita, dataehora
+        def get_ano_escolar(self):
+            return self.__ano_escolar
+
+        def set_ano_escolar(self, novo_ano_escolar):
+            self.__ano_escolar = novo_ano_escolar        
+            
+        def get_curso(self):
+            return self.__curso
+            
+        def set_curso(self, novo_curso):
+            self.__curso = novo_curso
+            
+        def cadastrar(self):
+            while True:
+                try:  
+                    nome = str(input("Digite seu nome: "))
+                    self.set_nome(nome)
+                    try:
+                        idade = int(input("Digite sua idade:"))
+                        self.set_idade(idade)
+                    except ValueError:
+                        print("Insira um número, sendo este do tipo inteiro.")
+                        print("Reiniciando o processo...")
+                        continue
+                    login_usuario = input("Cadastre um usuário: ")
+                    self.set_login_usuario(login_usuario)
+                    login_senha = input("Cadastre uma senha: ")
+                    self.set_login_senha(login_senha)
+                    self.__ano_escolar = input("Qual sua série?\n[1] - Primeiro ano\n[2] - Segundo ano\n[3] - Terceiro ano\n> ")
+                    if self.__ano_escolar != "1" and self.__ano_escolar != "2" and self.__ano_escolar != "3":
+                        print ("Coloque uma resposta válida.")
+                        continue
+                    print ("Digite seu curso o nome do seu curso:\nInformática\nQuímica\nEdificações\nEletrotécnica")
+                    self.__curso = input("Coloque sua resposta: ").upper()
+                    if self.__curso != 'INFORMÁTICA' and self.__curso != 'QUÍMICA' and self.__curso != 'EDIFICAÇÕES' and  self.__curso != 'ELETROTÉCNICA':
+                        print ("Coloque uma opção válida")
+                        continue
+                    else:
+                        print ("Cadastro Finalizado")
+                        break
+                except:
+                    print("Ocorreu algo inesperado. XO")
+                    
+        def escreverAvaliacao(self):
+            avalicacao_escrita = input("Escreva sua avaliação\n> ")
+            dataehora = datetime.now()
+            return avalicacao_escrita, dataehora
+except IndentationError:
+    print("ERRO DE IDENTAÇÃO, VERIFIQUE SE HÁ PARÊNTESES FALTANDO, VÍRGULA OU PONTOS TROCADOS")
