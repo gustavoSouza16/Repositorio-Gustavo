8a1d942d629ba3349d7ca6598dbd6ce025690d4f
Wellington Pereira <python6800@gmail.com>
2024-12-17 16:19:37 -0400

    Update ClasseProfessor.py
    tirei os ooo

--- SAP - Sistema de Avaliação de professores/ClasseProfessor.py
+++ SAP - Sistema de Avaliação de professores/ClasseProfessor.py
@@ -49,7 +49,7 @@ class Professor(UsuarioIfro):
         login_senha = input("Cadastre uma senha: ")
         self.set_login_senha(login_senha)
         
-        #Exceção para salárioooooo
+        #Exceção para salário
         while True:     
             try:
                 salario = int(input("Digite seu salário: "))
